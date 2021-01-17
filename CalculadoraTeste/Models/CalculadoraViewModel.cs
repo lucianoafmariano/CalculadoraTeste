9eb534864f1e512bf441e5c01e4f1e2b0debbf7d
Luciano Mariano <lucianoafmariano@gmail.com>
2021-01-17 14:36:37 -0300

    Primeiro commit da calculadora simples - 17/01/2021

---
+++ CalculadoraTeste/Models/CalculadoraViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalculadoraTeste.Models
+{
+    public class CalculadoraViewModel
+    {
+        /// <summary>
+        /// Número 1
+        /// </summary>
+        [Display(Name ="Número 1")]
+        public int Num1 { get; set; }
+
+        /// <summary>
+        /// Número 2
+        /// </summary>
+        [Display(Name = "Número 2")]
+        public int Num2 { get; set; }
+
+        /// <summary>
+        /// Operador + - * / 
+        /// </summary>
+        public string Operador { get; set; }
+
+        /// <summary>
+        /// Resultado da operação
+        /// </summary>
+        public int? Resultado { get; set; }
+    }
+}
